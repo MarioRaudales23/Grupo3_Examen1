065a17c1888dd2008ad65d97f7c6f6573feb5099
MarioRaudales23 <marioraudales33@unitec.edu>
2016-08-19 14:21:09 -0600

    Clase pokemon creada

---
+++ pokemon.cpp
@@ -0,0 +1,109 @@
+#include "pokemon.h"
+#include <string>
+
+pokemon::pokemon(){
+
+}
+pokemon::pokemon(string nombre,int nivel,string naturaleza,string tipoa,string tipob,int hp ,int ataque,int defensa,int ataqueesp,int defensesp,int rapidez,bool legendario){
+	nom_poke = nombre;
+	nivel_poke = nivel;
+	natur_poke = naturaleza;
+	tipo1_poke = tipoa;
+	tipo2_poke = tipob;
+	hp_poke = hp;
+	ataque_poke = ataque;
+	defensa_poke = defensa;
+	ataqesp_poke = ataqueesp;
+	defesp_poke = defensesp;
+	rapid_poke = rapidez;
+	legend_poke = legendario;
+}
+
+string pokemon::getnombre(){
+	return nom_poke;
+}
+
+void pokemon::setnombre(string nombre){
+	nom_poke = nombre;
+}
+
+int	pokemon::getnivel(){
+	return nivel_poke;
+}
+
+void pokemon::setnivel(int nivel){
+	nivel_poke = nivel;
+}
+string pokemon::gettipo1(){
+	return tipo1_poke;
+}
+
+void pokemon::settipo1(string tipoa){
+	tipo1_poke = tipoa;
+}
+string pokemon::gettipo2(){
+	return tipo2_poke;
+}
+
+void pokemon::settipo2(string tipob){
+	tipo2_poke = tipob;
+}
+string pokemon::getnaturaleza(){
+	return natur_poke;
+}
+
+void pokemon::setnaturaleza(string naturaleza){
+	natur_poke = naturaleza;
+}
+int	pokemon::gethp(){
+	return hp_poke;
+}
+
+void pokemon::sethp(int hp){
+	hp_poke = hp;
+}
+int	pokemon::getataque(){
+	return ataque_poke;
+}
+
+void pokemon::setataque(int ataque){
+	ataque_poke = ataque;
+}
+int	pokemon::getdefensa(){
+	return defensa_poke;
+}
+
+void pokemon::setdefensa(int defensa){
+	nivel_poke = nivel;
+}
+int	pokemon::getdefensaesp(){
+	return defesp_poke;
+}
+
+void pokemon::setdefensaesp(int defensesp){
+	defesp_poke = defensesp;
+}
+int	pokemon::getataquesp(){
+	return ataqesp_poke;
+}
+
+void pokemon::setataquesp(int ataqueesp){
+	ataqesp_poke = ataqueesp;
+}
+int	pokemon::getrapidez(){
+	return rapid_poke;
+}
+
+void pokemon::setrapidez(int rapidez){
+	rapid_poke = rapidez;
+}
+bool pokemon::getlegendario(){
+	return legend_poke;
+}
+
+void pokemon::setlegendario(int legendario){
+	legend_poke = legendario;
+}
+pokemon::~pokemon(){
+
+}
---
+++ pokemon.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <string>
+class pokemon{
+	private:
+		string nom_poke;
+		int nivel_poke;
+		string natur_poke;
+		string tipo1_poke;
+		string tipo2_poke;
+		int hp_poke;
+		int ataque_poke;
+		int defensa_poke;
+		int ataqesp_poke;
+		int defesp_poke;
+		int rapid_poke;
+		bool legend_poke;
+	public:
+		pokemon(string,int,string,string,string,int,int,int,int,int,int,bool);	
+		pokemon();
+		~pokemon();
+		string getnombre();
+		void setnombre(string);
+		string getnaturaleza();
+		void setnaturaleza(string);
+		int getnivel();
+		void setnivel(int);
+		void settipo1(string);
+		string gettipo1();
+		void settipo2(string);
+		string gettipo2();
+		int gethp();
+		void sethp(int);
+		int getataque();
+		void setataque(int);
+		int getdefensa();
+		void setdefensa(int);
+		int getdefensaesp();
+		void setdefensaesp(int);
+		int getataquesp();
+		void setataquesp(int);
+		int getrapidez();
+		void setrapidez(int);
+		bool getlegendario();
+		void setlegendario(bool);
+};
